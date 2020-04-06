a09258e9796c5266806f70cbbf8ab286e90dbd9e
joshy23 <58797123+joshy56@users.noreply.github.com>
2020-04-05 22:42:25 -0300

    Basic files
    Basic class and files added

---
+++ src/com/joshy23/fragmentedstaff/FragmentedStaff.java
@@ -0,0 +1,15 @@
+package com.joshy23.fragmentedstaff;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class FragmentedStaff extends JavaPlugin {
+
+    public void onEnable() {
+
+    }
+
+    public void onDisable() {
+
+    }
+
+}
